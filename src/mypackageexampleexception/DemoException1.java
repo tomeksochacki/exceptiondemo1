6fcc28d110293e01024f4a34a5bf44173f6ca445
tomek <tomeksochacki@wp.pl>
2020-07-01 11:36:34 +0200

    Initialize repository

---
+++ src/mypackageexampleexception/DemoException1.java
@@ -0,0 +1,17 @@
+package mypackageexampleexception;
+
+public class DemoException1 {
+    public static void main(String[] args) {
+        int[] tab = new int[4];
+        try {
+            System.out.println("Before generate exception.");
+
+            tab[10] = 7;
+            System.out.println("This statement is not println.");
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Index out of array bounds.");
+        }
+        System.out.println("After catch.");
+    }
+}
